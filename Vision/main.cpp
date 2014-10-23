8c77aea2a1f807fad512de2d4949588b4524043a
Leph <leph.quentinrouxel@gmail.com>
2014-10-23 14:52:23 +0200

    Fist test files

---
+++ Vision/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <opencv.hpp>
+
+int main()
+{
+    cv::VideoCapture camera(0);
+    if (camera.isOpened()) {
+        std::cerr << "Unable to open camera" << std::endl;
+        return 1;
+    }
+
+    cv::namedWindow("win");
+
+    while (true) {
+        cv::Mat frame;
+        camera >> frame;
+        cv::imshow("win", frame);
+        if (cv::waitKey(10) != 1) break;
+    }
+
+    return 0;
+}
+
