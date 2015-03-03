f6082337c72832c53d12a50257f28d73fd8020bb
Gregwar <g.passault@gmail.com>
2015-03-03 14:18:30 +0100

    Example

--- AI/main.cpp
+++ AI/main.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <iostream>
 #include <HardwareClient.h>
 #include <motors.h>
@@ -8,6 +9,8 @@ using namespace std;
 
 int main()
 {
+    float dx=0, dy=0, turn=0;
+    float sdx=0, sdy=0, sturn=0;
     float t = 0;
     HardwareClient client;
     client.connect();
@@ -15,6 +18,7 @@ int main()
     locomotion_init();
     locomotion_set_dx(0);
     locomotion_set_dy(0);
+    locomotion_set_turn(0);
 
     while (true) {
         t += 0.02;
@@ -27,20 +31,43 @@ int main()
         // clever();
 
         if (img.width && img.height) {
+            img.writePPM("out.ppm");
             float xM = 0.0;
             int c = 0;
             for (unsigned int x=0; x<img.width; x++) {
                 for (unsigned int y=0; y<img.height; y++) {
                     if (img.getR(x, y) > 200 && img.getG(x,y)<60 && img.getB(x,y)<60) {
-                    img.setColor(x,y,0x0000ff);
                         xM += x;
                         c++;
                     }
                 }
             }
-            printf("%g\n", xM/c);
-            img.writePPM("out.ppm");
+            xM /= c;
+            float xPos = (xM-(img.width/2))/(img.width/2);
+
+            if (xPos==xPos && fabs(xPos) < 0.2) {
+                turn = 0;
+                dx = 60;
+                dy = -60;
+            } else {
+                dx = 0;
+                dy = 0;
+
+                if (c > 10) {
+                    turn = xPos*30;
+                } else {
+                    turn = 30;
+                }
+            }
         }
+    
+        // Smoothing locomotion
+        sdx = sdx*0.8+dx*0.2;
+        sdy = sdy*0.8+dy*0.2;
+        sturn = sturn*0.8+turn*0.2;
+        locomotion_set_dx(sdx);
+        locomotion_set_dy(sdy);
+        locomotion_set_turn(sturn);
 
         // Sending angles to the hardware server
         for (unsigned int k=0; k<4; k++) {
